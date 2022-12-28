c323406b8e397d4e844427650ba5d6adef6f884e
lioc3sar <juliocesaoguimaraes@gmail.com>
2022-12-28 16:02:42 -0300

    JSX Arrays 1

--- ambiente-do-curso/appreact/src/App.js
+++ ambiente-do-curso/appreact/src/App.js
@@ -1,81 +1,30 @@
 import React from 'react';
 
-const titulo = <h1>Esse e um titulo</h1>;
-
 const App = () => {
-  const random = Math.random();
-  const ativo = true;
+  // const produtos = ['Notebook', 'Tablet', 'Smartphone']
+  // const produtos = [<li key="e1">Notebook</li>,<li key="e1">Smartphone</li>]
+  // return <div>{produtos}</div>
 
-  function mostrarNome(sobrenome) {
-    return 'Julio ' + sobrenome;
-  }
+  // const filmes = ['Before Sunrise', 'Before Sunset', 'Before Midnight'];
+  // return <ul>{filmes.map((filme) =>( <li key={filme}>{filme}</li>))}</ul>
 
-  const carro = {
-    marca: 'Ford',
-    rodas: 4,
-  };
+  const livros = [
+    { nome: 'A Game of Thrones', ano: 1996 },
+    { nome: 'A Clash of Kings', ano: 1998 },
+    { nome: 'A Storm of Swords', ano: 2000 },
+  ];
 
-  const estiloP = {
-    color: 'blue',
-    fontSize: '2rem',
-  };
-
-  const luana = {
-    cliente: 'Luana',
-    idade: 27,
-    compras: [
-      { nome: 'Notebook', preco: 'R$ 2500' },
-      { nome: 'Geladeira', preco: 'R$ 3000' },
-      { nome: 'Smartphone', preco: 'R$ 1500' },
-    ],
-    ativa: true,
-  };
-
-  const mario = {
-    cliente: 'Mario',
-    idade: 31,
-    compras: [
-      { nome: 'Notebook', preco: 'R$ 2500' },
-      { nome: 'Geladeira', preco: 'R$ 3000' },
-      { nome: 'Smartphone', preco: 'R$ 1500' },
-      { nome: 'Guitarra', preco: 'R$ 3500' },
-    ],
-    ativa: false,
-  };
-
-  const dados = luana;
-  const total = dados.compras
-    .map((item) => Number(item.preco.replace('R$', '')))
-    .reduce((a, b) => a + b);
-  console.log(total);
-
+  // return <ul>{livros.map(({nome,ano})=>( <li key={nome}>{nome}, {ano}</li>))}</ul>
   return (
-    // <a className="ativo" href='https://www.origamid.com' title='Isso e um site'>Origamid</a>
-    // <>
-    // <label htmlFor="nome">Nome</label>
-    // <input type="text" id="nome" />
-    // </>
-    /* {titulo}
-    <p>{true ? 'asdasd' : 'afgfgf'} - {10}</p>
-    <p>{mostrarNome('Cesar')}</p>
-    <p style={estiloP}>{new Date().getFullYear()}</p>
-    <p>{carro.marca}</p>
-    <p>{carro.rodas}</p>
-    <p className={ativo ? 'ativo' : 'inativo'}>{random * 100} / 50</p> */
-
-    <>
-      <p>Nome {dados.cliente}</p>
-      <p>Idade {dados.idade}</p>
-      <p>
-        Situacao:
-        <span style={{ color: dados.ativa ? 'green' : 'red' }}>
-          {dados.ativa ? 'Ativa' : 'Inativa'}
-        </span>
-      </p>
-      <p>Total: {total}</p>
-
-      {total > 10000 && <p>'Voce esta gastando muito'</p>}
-    </>
+    <ul>
+      {livros
+        .filter(({ ano }) => ano >= 1998)
+        .map(({ nome, ano }) => (
+          <li key={nome}>
+            {nome}, {ano}
+          </li>
+        ))}
+    </ul>
   );
 };
 
