98aab7da4164f50f9abf717625185c15068b02f5
lioc3sar <juliocesaoguimaraes@gmail.com>
2022-12-28 10:48:50 -0300

    Webpack e Babel

---
+++ app/src/App.js
@@ -0,0 +1,9 @@
+import React from "react";
+import "./App.css"
+
+const App = () => {
+  return <a href="https://www.origamid.com">Origamid</a>;
+  // return React.createElement('a',{href: 'http://www.origamid.com'}, 'Origamid');
+}
+
+export default App;
---
+++ app/webpack.config.js
@@ -0,0 +1,33 @@
+module.exports = {
+  // indica que o devserver deve utilizar a raiz para servir o html.
+  // atualização de 2021
+  devServer: {
+    static: "./"
+  },
+   // Nos módulos
+   module: {
+    // Aplique as seguintes regras
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        // Nos arquivos que terminam ($) com .js
+        test: /\.js$/,
+        // Não procure nada em node_modules
+        exclude: /node_modules/,
+        // Use o seguinte:
+        use: {
+          // Babel
+          loader: 'babel-loader',
+          // Com as opções padrões para o React
+          options: {
+            presets: ['@babel/preset-react'],
+          },
+        },
+      },
+    ],
+  },
+  
+}
