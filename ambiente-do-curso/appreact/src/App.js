325e213f5ad5ad08c95ba9878b3956791b9ad1c1
lioc3sar <juliocesaoguimaraes@gmail.com>
2022-12-31 10:37:04 -0300

    useState 2

--- ambiente-do-curso/appreact/src/App.js
+++ ambiente-do-curso/appreact/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import ButtonModal from './ButtonModal';
 import Modal from './Modal';
 
@@ -6,14 +6,29 @@ import Modal from './Modal';
 const App = () => {
   // const [ativo,setAtivo] = React.useState(false);
   // const [dados,setDados] = React.useState({nome:'Julio',idade:23})
-
   // function handleClick(){
   //   setAtivo(!ativo)
   //   setDados({...dados,faculdade:"Possui Faculdade"})
-
   // }
 
-  const [modal,setModal] = React.useState(false);
+  // const [modal,setModal] = React.useState(() => {
+  //   const ativo = window.localStorage.getItem('ativo');
+  //   return ativo;
+  // });
+  // const [items,setItems] = React.useState('Teste');
+  // function handleClick(){
+  //   setItems('Outros');
+  // }
+
+
+  // const [contar,setContar] = React.useState(1);
+  // const [items,setItems] = React.useState(['Item 1'])
+  // function handleClick(){
+  //   setContar(contar + 1)
+  //   setItems((items) => [...items,"Item" + (contar+1)])
+  // }
+  const [dados,setDados] = useState({});
+
  
   return (
   //  <div>
@@ -22,11 +37,21 @@ const App = () => {
   //   <p>{dados.faculdade}</p>
   //   <button onClick={handleClick}>{ativo? 'Ativo' : 'Inativo'}</button>
   //  </div>
-  <div>
-    <div>{modal ? 'Modal aberto' : 'Modal fechado'}</div>
-    <Modal modal={modal} setModal={setModal}/>
-    <ButtonModal setModal={setModal}/>
-  </div>
+
+  // <div>
+  //   <p>{items}</p>
+  //   <button onClick={handleClick}>Clicar</button>
+  //   <div>{modal ? 'Modal aberto' : 'Modal fechado'}</div>
+  //   <Modal modal={modal} setModal={setModal}/>
+  //   <ButtonModal setModal={setModal}/>
+  // </div>
+
+  // <div>
+  //   {items.map(item => <li key={item}>{item}</li>)}
+  //   <button onClick={handleClick}>{contar}</button>
+  // </div>
+
+  <div></div>
   );
 
 };
--- ambiente-do-curso/appreact/src/ButtonModal.js
+++ ambiente-do-curso/appreact/src/ButtonModal.js
@@ -1,8 +1,13 @@
 import React from 'react'
 
 const ButtonModal = ({setModal}) => {
+
+  function handleClick(){
+    setModal((ativo) => !ativo)
+  }
+
   return (
-    <button onClick={() => setModal(true)}>Abrir</button>
+    <button onClick={handleClick}>Abrir</button>
   )
 }
 
