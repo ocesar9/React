0fa6f65e04966c076202a34ebb5babf7cf095b66
lioc3sar <juliocesaoguimaraes@gmail.com>
2023-01-01 22:01:51 -0300

    useeffect 2

--- ambiente-do-curso/appreact/src/App.js
+++ ambiente-do-curso/appreact/src/App.js
@@ -1,43 +1,30 @@
-import React from 'react';
+import React from 'react'
 import Produto from './Produto';
 
 const App = () => {
-  const [ativo,setAtivo] = React.useState(false)
+  const [produto, setProduto] = React.useState(null);
+  
+  React.useEffect(() =>{
+    const produtoLocal = window.localStorage.getItem('produto')
+    if(produtoLocal !== null) setProduto(produtoLocal)
+  },[])
 
-  // const [contar, setContar] = React.useState(0);
-  // const [dados,setDados] = React.useState(null);
+  React.useEffect(() => {
+    if(produto !== null) window.localStorage.setItem('produto', produto);
+  },[produto])
 
-  // React.useEffect(() => {
-  //   fetch('https://ranekapi.origamid.dev/json/api/produto/notebook')
-  //   .then((response) => response.json())
-  //   .then((json) => setDados(json));
-  // }, [])
-
-  // React.useEffect(() => {
-  //   console.log('Executou');
-  // },[])
-
-  // const titulo = 'Total ';
-
-  // React.useEffect(() => {
-  //   document.title = titulo + contar;
-  // },[contar])
+  function handleClick({target}) {
+    setProduto(target.innerText)
+  }
 
   return (
-    // <div>
-    //   {dados && (<div>
-    //     <h1>{dados.nome}</h1>
-    //     <p>{dados.preco}</p>
-    //   </div>)}
-    //   <button onClick={() => setContar(contar +1)}>{contar}</button>
-    // </div>
-
-
     <div>
-      {ativo && <Produto />}
-      <button onClick={() => setAtivo(!ativo)}>Ativar</button>
+      {produto && <h1>Preferencia {produto}</h1>}
+      <button onClick={handleClick} style={{marginRight: '1rem'}}>notebook</button>
+      <button onClick={handleClick}>smartphone</button>
+      {produto && <Produto produto={produto}/>}
     </div>
-  );
-};
+  )
+}
 
-export default App;
+export default App
--- ambiente-do-curso/appreact/src/Produto.js
+++ ambiente-do-curso/appreact/src/Produto.js
@@ -1,20 +1,21 @@
 import React from 'react'
 
-const Produto = () => {
+const Produto = ({produto}) => {
+  const [dados,setDados] = React.useState(null)
+  
+  React.useEffect(() => {
+    if(produto !== null)
+    fetch(`https://ranekapi.origamid.dev/json/api/produto/${produto}`)
+    .then((response) => response.json())
+    .then(json => setDados(json))
+  },[produto])
 
- React.useEffect(() =>{
-  function handleScroll(e){
-    console.log(e);
-  }
-  window.addEventListener('scroll', handleScroll)
-  return () => {
-    window.removeEventListener('scroll',handleScroll)
-  }
- },[])
+  if(dados === null) return null;
 
   return (
-    <div style={{height: "200vh"}}>
-      <p>Meu produto</p>
+    <div>
+      <h1>{dados.nome}</h1>
+      <p>{dados.preco}</p>
     </div>
   )
 }
