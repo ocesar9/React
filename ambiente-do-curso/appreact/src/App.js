273717197ea3af1d63e16069ac919f25b26a5f42
lioc3sar <juliocesaoguimaraes@gmail.com>
2023-01-05 10:28:23 -0300

    Input 1

--- ambiente-do-curso/appreact/src/App.js
+++ ambiente-do-curso/appreact/src/App.js
@@ -1,18 +1,50 @@
-import React from 'react'
-
-function useNumeroAleatorio(){
-  React.useEffect(() => {
-    document.title = Math.random();
-  },[])
-}
+import React from 'react';
 
 const App = () => {
-  useNumeroAleatorio();
+
+  const [form,setForm] = React.useState({
+    nome:'',
+    email: '',
+  })
+  // const [nome, setNome] = React.useState('');
+  // const [email, setEmail] = React.useState('');
+
+  function handleSubmit(event){
+    console.log(event);
+  }
+
+  function handleChange({target}){
+    const {id,value} = target;
+    console.log(id,value);
+    setForm({...form,[id]:value})
+
+  }
+
   return (
-    <div>
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="nome">Nome</label>
+      <input
+        id='nome'
+        type="text"
+        name='nome'
+        value={form.nome}
+        onChange={handleChange}
+      />
+      {form.nome}
+      <br />
+      <label htmlFor="email">Email</label>
+      <input
+        id='email'
+        type="text"
+        name='email'
+        value={form.email}
+        onChange={handleChange}
+        />
+        {form.email}
+        <br />
+      <button>Enviar</button>
+    </form>
+  );
+};
 
-    </div>
-  )
-}
-
-export default App
+export default App;
