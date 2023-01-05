7061fa25baa3b7a8c03756c985c0a4e6dc170b5e
lioc3sar <juliocesaoguimaraes@gmail.com>
2023-01-05 14:11:38 -0300

    Radio

--- ambiente-do-curso/appreact/src/App.js
+++ ambiente-do-curso/appreact/src/App.js
@@ -1,17 +1,37 @@
 import React from 'react';
 
 const App = () => {
-  const [select, setSelect] = React.useState(null);
+
+  const [produto,setProduto] = React.useState('');
+  const [cor,setCor] = React.useState('');
+
+  function handleChange({target}){
+    setProduto(target.value)
+  }
 
   return (
     <form>
-      <select value={select} onChange={({target}) => setSelect(target.value)} id="produtos">
-        <option deisabled value="">Selecione</option>
-        <option value="notebook">Notebook</option>
-        <option value="smartphone">Smartphone</option>
-        <option value="tablet">Tablet</option>
-      </select>
-      {select}
+      <h2>Produtos</h2>
+      <label htmlFor="">
+        <input type="radio" name="produto" value="smartphone" onChange={handleChange} checked={produto === "smartphone"}/>
+        Smartphone
+      </label>
+      <label htmlFor="">
+        <input type="radio" name="produto" value="notebook" onChange={handleChange} checked={produto === "notebook"}/>
+        Notebook
+      </label>
+      {produto}
+      
+      <h2>Cores</h2>
+      <label htmlFor="">
+        <input type="radio" name="cor" value="azul" onChange={({target}) => setCor(target.value)} checked={cor === "azul"}/>
+        Azul
+      </label>
+      <label htmlFor="">
+        <input type="radio" name="cor" value="vermelho" onChange={({target}) => setCor(target.value)} checked={cor === "vermelho"}/>
+        Vermelho
+      </label>
+      {cor}
     </form>
   );
 };
