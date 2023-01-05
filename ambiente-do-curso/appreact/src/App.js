5df87b97367dcd3c7f058768ba406bea540a8b3b
lioc3sar <juliocesaoguimaraes@gmail.com>
2023-01-05 17:54:10 -0300

    Componentes Select

--- ambiente-do-curso/appreact/src/App.js
+++ ambiente-do-curso/appreact/src/App.js
@@ -1,12 +1,15 @@
 import React from 'react'
-import Input from './Form/input'
+import Input from './Form/Input'
+import Select from './Form/Select';
 
 const App = () => {
   const [nome,setNome] = React.useState('');
   const [email,setEmail] = React.useState('');
+  const [produto,setProduto] = React.useState('');
 
   return (
     <form>
+      <Select options={['smarthphone','tablet']} value={produto} setValue={setProduto}/>
       <Input label="Nome" id="nome" value={nome} setValue={setNome}/>
       <Input label="Email" id="email" value={email} setValue={setEmail} required/>
       <button>Enviar</button>
@@ -14,4 +17,4 @@ const App = () => {
   )
 }
 
-export default App
+export default App;
