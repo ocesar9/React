ea19d4360fa5030e6fc51602955d4b4fa5916921
lioc3sar <juliocesaoguimaraes@gmail.com>
2023-01-10 10:08:12 -0300

    Styled Components

--- ambiente-do-curso/appreact/src/App.js
+++ ambiente-do-curso/appreact/src/App.js
@@ -1,10 +1,73 @@
 import React from 'react'
-import Produto from './Components/Produto'
+import styled from 'styled-components'
 
+const ProdutosContainer = styled.div `
+  display: flex;
+
+`
+
+const Produto = styled.div`
+  flex: 1;
+`
+
+const Titulo = styled.h1`
+  font-size:2rem;
+  color: tomato;
+`
+const Paragrafo = styled.p`
+  font-size: 1.5rem;
+  color: green;
+`
+
+const Preco = styled.p`
+  /* background: ${({cor}) => cor}; */
+  background: hsl(${Math.random() * 360}, 100%,50%);
+  color: white;
+`
+
+const Comprar = styled.button`
+  background: ${({ativo}) => ativo ? '#000' : '#fff'};
+  font-size: 1rem;
+  border: 2px solid #000;
+  border-radius: 5px;
+  padding: 0.5rem;
+  color: white;
+  color: ${({ativo}) => ativo ? '#fff' : '#000'};
+  cursor: pointer;
+  &:hover{
+    background: tomato;
+  }
+`
 const App = () => {
+
+  // function template(value,total){
+  //   console.log(value);
+  // }
+  // const total = 100
+  // template`Isso e ${total} um teste`
+
+  const [ativo,setAtivo] = React.useState(false);
+
+  function handleClick(){
+    setAtivo(!ativo)
+  }
+
+
   return (
     <div>
-      <Produto/>
+      <Comprar ativo={ativo} onClick={handleClick}>Compre aqui</Comprar>
+      <ProdutosContainer>
+        <Produto>
+          <Titulo>Notebook</Titulo>
+          <Paragrafo>Meu texto e esse.</Paragrafo>
+          <Preco>R$ 2000</Preco>
+        </Produto>
+        <Produto>
+          <Titulo>Smartphone</Titulo>
+          <Paragrafo>Meu texto e esse.</Paragrafo>
+          <Preco>R$ 1000</Preco>
+        </Produto>
+      </ProdutosContainer>
     </div>
   )
 }
