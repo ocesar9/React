af0dc5079cacf520d31a2508436a5009d9ca8cbe
lioc3sar <juliocesaoguimaraes@gmail.com>
2023-01-10 11:37:44 -0300

    Animações 2

--- ambiente-do-curso/appreact/src/App.js
+++ ambiente-do-curso/appreact/src/App.js
@@ -1,14 +1,26 @@
 import React from 'react'
-import Produto from './Produto'
-import './App.css'
+import Slide from './Slide';
 
 const App = () => {
-  const [ativar,setAtivar] = React.useState(false);
+  const slides = [
+    {
+      id: 'slide1',
+      text:'Slide 1'
+    },
+    {
+      id: 'slide2',
+      text:'Slide 2'
+    },
+    {
+      id: 'slide3',
+      text:'Slide 3'
+    }
+  ]
+
 
   return (
     <div>
-      <button onClick={() => setAtivar(!ativar)}>Ativar</button>
-      {ativar && <Produto/>}
+      <Slide slides={slides}/>
     </div>
   )
 }
