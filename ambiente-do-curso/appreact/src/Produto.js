bade99ba8888ca27902f4c166b0d491d85877b41
lioc3sar <juliocesaoguimaraes@gmail.com>
2023-01-11 09:49:39 -0300

    useParams

---
+++ ambiente-do-curso/appreact/src/Produto.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { useParams,useLocation } from 'react-router-dom'
+
+const Produto = () => {
+  const params = useParams();
+
+  return (
+    <div>
+      <h1>Produto : {params.id}</h1>
+    </div>
+  )
+}
+
+export default Produto
