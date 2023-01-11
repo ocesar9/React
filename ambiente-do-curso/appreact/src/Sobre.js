9d47b6dcd5dee0ea1248008c22a0f5699a208e77
lioc3sar <juliocesaoguimaraes@gmail.com>
2023-01-11 11:35:22 -0300

    Head

--- ambiente-do-curso/appreact/src/Sobre.js
+++ ambiente-do-curso/appreact/src/Sobre.js
@@ -1,8 +1,10 @@
 import React from 'react'
+import Head from './Head'
 
 const Sobre = () => {
   return (
     <div>
+      <Head title="Sobre" description='Essa e a descricao da Home'/>
       <h1>Sobre</h1>
       <p>Essa e a sobre</p>
     </div>
