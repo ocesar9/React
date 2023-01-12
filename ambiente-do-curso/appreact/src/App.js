8e8e26a121d635656fcdb37e61ee718b446ff21b
lioc3sar <juliocesaoguimaraes@gmail.com>
2023-01-12 14:40:06 -0300

    PropTypes

--- ambiente-do-curso/appreact/src/App.js
+++ ambiente-do-curso/appreact/src/App.js
@@ -1,33 +1,14 @@
 import React from 'react'
-import {BrowserRouter,Routes,Route} from 'react-router-dom'
-import Header from './Header'
-import Home from './Home'
-import Login from './Login'
-import NaoEncontrada from './NaoEncontrada'
-import Produto from './Produto'
-import Sobre from './Sobre'
+import Button from './Button'
 
-import ProdutoAvaliacao from './ProdutoAvaliacao';
-import ProdutoCustomizado from './ProdutoCustomizado';
-import ProdutoDescricao from './ProdutoDescricao';
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <Header/>
-      <Routes>
-        <Route path='/' element={<Home/>}/>
-        <Route path='sobre' element={<Sobre/>}/>
-        <Route path='login' element={<Login/>}/>
-        <Route path='produto/:id/*' element={<Produto/>}>
-          <Route path='/' element={<ProdutoDescricao/>} />
-          <Route path='avaliacao' element={<ProdutoAvaliacao/>} />
-          <Route path='customizado' element={<ProdutoCustomizado/>} />
-        </Route>
-        <Route path='*' element={<NaoEncontrada/>}/>
-      </Routes>
-    </BrowserRouter>
+    <div>
+      <Button width={100}>Clique aqui</Button>
+    </div>
   )
 }
+
 
 export default App
