d811cb97271892fcd73a83d34b8cb8f1cebd3a66
lioc3sar <juliocesaoguimaraes@gmail.com>
2023-01-12 14:51:49 -0300

    Lazy e Suspense

--- ambiente-do-curso/appreact/src/App.js
+++ ambiente-do-curso/appreact/src/App.js
@@ -1,14 +1,19 @@
-import React from 'react'
-import Button from './Button'
-
+import React from 'react';
+const Contato = React.lazy(() => import('./Contato'));
 
 const App = () => {
+  const [ativar, setAtivar] = React.useState(false);
+
   return (
     <div>
-      <Button width={100}>Clique aqui</Button>
+      {ativar && (
+        <React.Suspense fallback={<div>Carregando...</div>}>
+          <Contato />
+        </React.Suspense>
+      )}
+      <button onClick={() => setAtivar(true)}>Ativar</button>
     </div>
-  )
-}
+  );
+};
 
-
-export default App
+export default App;
