2a58b82d85f42c808850c97db9054f0b934130a2
lioc3sar <juliocesaoguimaraes@gmail.com>
2023-01-12 20:48:31 -0300

    useReducer

--- ambiente-do-curso/appreact/src/App.js
+++ ambiente-do-curso/appreact/src/App.js
@@ -1,14 +1,48 @@
 import React from 'react'
-import Header from './Header'
+import Exemplo from './Exemplo';
+
+function reducer(state,action){
+  console.log(state);
+  console.log(action);
+
+  // if(action === 'aumentar'){
+  //   return state+1
+  // }
+  // if(action === 'dirminuir'){
+  //   return state-1;
+  // }
+
+  switch (action) {
+    case 'aumentar':
+      return state+1;
+    case 'dirminuir':
+      return state-2;
+    default: 
+    throw new Error('Invalid action')
+  }
+
+}
 
 const App = () => {
 
-  const [contar,setContar] = React.useState(0);
+  const [state,dispatch] = React.useReducer(reducer,0);
+
+  // function aumentar(){
+  //   setContar(contar+1)
+  // }
+  
+  // function dirminuir(){
+  //   setContar(contar-1)
+  // }
 
   return (
     <div>
-      <Header />
-      <button onClick={() => setContar(contar+1)}>{contar}</button>
+      {/* <button onClick={aumentar}>+</button>
+      <button onClick={dirminuir}>-</button> */}
+      <button onClick={() => dispatch('aumentar')}>+</button>
+      <button onClick={() => dispatch('dirminuir')}>-</button>
+      <p>{state}</p>
+      <Exemplo/>
     </div>
   )
 }
