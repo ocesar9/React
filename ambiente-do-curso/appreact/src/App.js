ff16eb312c370c59e2ae25e6d8ce87904e01025b
lioc3sar <juliocesaoguimaraes@gmail.com>
2023-01-12 21:18:39 -0300

    Classes

--- ambiente-do-curso/appreact/src/App.js
+++ ambiente-do-curso/appreact/src/App.js
@@ -1,50 +1,12 @@
-import React from 'react'
-import Exemplo from './Exemplo';
-
-function reducer(state,action){
-  console.log(state);
-  console.log(action);
-
-  // if(action === 'aumentar'){
-  //   return state+1
-  // }
-  // if(action === 'dirminuir'){
-  //   return state-1;
-  // }
-
-  switch (action) {
-    case 'aumentar':
-      return state+1;
-    case 'dirminuir':
-      return state-2;
-    default: 
-    throw new Error('Invalid action')
-  }
-
-}
+import React from 'react';
+import Produtos from './Produto';
 
 const App = () => {
+  const [ativar, setAtivar] = React.useState(true);
 
-  const [state,dispatch] = React.useReducer(reducer,0);
+  return <div>{ativar && <Produtos titulo="Esses sao os Produtos" />}
+    <button onClick={() => setAtivar(!ativar)}>Toggle</button>
+  </div>;
+};
 
-  // function aumentar(){
-  //   setContar(contar+1)
-  // }
-  
-  // function dirminuir(){
-  //   setContar(contar-1)
-  // }
-
-  return (
-    <div>
-      {/* <button onClick={aumentar}>+</button>
-      <button onClick={dirminuir}>-</button> */}
-      <button onClick={() => dispatch('aumentar')}>+</button>
-      <button onClick={() => dispatch('dirminuir')}>-</button>
-      <p>{state}</p>
-      <Exemplo/>
-    </div>
-  )
-}
-
-export default App
+export default App;
