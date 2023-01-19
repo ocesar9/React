f829be96f842538811990293848c7d3f1b996dc2
lioc3sar <juliocesaoguimaraes@gmail.com>
2023-01-19 11:38:53 -0300

    Header

--- projeto-final/dogs/src/components/Header.js
+++ projeto-final/dogs/src/components/Header.js
@@ -1,16 +1,19 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import styles from './Header.module.css'
+import { ReactComponent as Dogs } from '../Assets/dogs.svg'
 
 const Header = () => {
   return (
-    <div className={styles.header}>
-      <nav className='container'>
-          <Link to='/'>Home</Link>
-          <Link to='/'>Login / Criar</Link>
+    <header className={styles.header}>
+      <nav className={`${styles.nav} container`}>
+          <Link className={styles.logo} to='/' aria-label='Dogs - Home'>
+            <Dogs />
+          </Link>
+          <Link className={styles.login} to='/'>Login / Criar</Link>
 
       </nav>
-    </div>
+    </header>
   )
 }
 
