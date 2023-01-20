a15efc771bc97f8ac7c611b482ee08c9747fd417
lioc3sar <juliocesaoguimaraes@gmail.com>
2023-01-20 16:08:52 -0300

    useForm

--- projeto-final/dogs/src/components/login/LoginForm.js
+++ projeto-final/dogs/src/components/login/LoginForm.js
@@ -1,33 +1,38 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import useForm from '../../hooks/useForm';
 import Button from '../forms/Button';
 import Input from '../forms/Input';
 
 const LoginForm = () => {
-  const [username, setUsername] = React.useState('');
-  const [password, setPassword] = React.useState('');
+    const username = useForm();
+    const password = useForm();
+
+
 
   function handleSubmit(event) {
     event.preventDefault();
-    fetch('https://dogsapi.origamid.dev/json/jwt-auth/v1/token', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ username, password }),
-    })
-    .then((response) => {
-      return response.json();
-    })
-    .then((json) => console.log(json));
+    if(username.validate() && password.validate()) {
+      fetch('https://dogsapi.origamid.dev/json/jwt-auth/v1/token', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(),
+      })
+      .then((response) => {
+        return response.json();
+      })
+      .then((json) => console.log(json));
+    }
   }
 
   return (
     <div>
       <h1>Login</h1>
       <form action="" onSubmit={handleSubmit}>
-        <Input label='Usuario' type='text' name='username'/>
-        <Input label='Senha' type='password' name='password'/>
+        <Input label='Usuario' type='text' name='username' {...username}/>
+        <Input label='Senha' type='password' name='password' {...password}/>
         <Button>Entrar</Button>
       </form>
       <Link to="/login/criar">Cadastrado</Link>
