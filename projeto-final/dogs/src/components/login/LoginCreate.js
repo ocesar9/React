fd1915861216dd90b24d18ea2eded7fb6dbc8e8f
ocesar9 <juliocesaoguimaraes@gmail.com>
2023-01-31 14:14:56 -0300

    Login Criar

--- projeto-final/dogs/src/components/login/LoginCreate.js
+++ projeto-final/dogs/src/components/login/LoginCreate.js
@@ -1,8 +1,41 @@
 import React from 'react'
+import { USER_POST } from '../../api';
+import useForm from '../../hooks/useForm';
+import { UserContext } from '../../UserContext';
+import Button from '../forms/Button';
+import Input from '../forms/Input';
 
 const LoginCreate = () => {
+
+  const username = useForm();
+  const email = useForm('email');
+  const password = useForm('password');
+
+  const {userLogin} = React.useContext(UserContext);
+
+  async function handleSubmit(event){
+    event.preventDefault();
+    const {url,options} = USER_POST({
+      username: username.value,
+      password: password.value,
+      email: email.value
+    })
+    const response = await fetch(url,options);
+    if(response.ok) userLogin(username.value, password.value)
+    console.log(response);
+
+  }
+
   return (
-    <div>Login Criar</div>
+    <section className='animeLeft'>
+      <h1 className='title'>Cadastre-se</h1>
+      <form onSubmit={handleSubmit}>
+          <Input label="Usuario" type="text" name="username" {...username}/>
+          <Input label="Email" type="email" name="email" {...email}/>
+          <Input label="Senha" type="password" name="password" {...password}/>
+          <Button>Cadastrar</Button>
+      </form>
+    </section>
   )
 }
 
--- projeto-final/dogs/src/hooks/useForm.js
+++ projeto-final/dogs/src/hooks/useForm.js
@@ -6,6 +6,10 @@ const types = {
       /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
     message: 'Preencha um email valido',
   },
+  password: {
+    regex: /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}$/,
+    message: 'A senha precisa ter 1 caracter maisuclo, 1 minusculo e 1 digito. Com no minimo 8 caracteres'
+  }
 };
 
 const useForm = (type) => {
