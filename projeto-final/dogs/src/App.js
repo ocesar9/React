5a31904914095180a3a6ee278d0d94a1c9e8bee2
ocesar9 <juliocesaoguimaraes@gmail.com>
2023-01-31 21:16:44 -0300

    Protected Route

--- projeto-final/dogs/src/App.js
+++ projeto-final/dogs/src/App.js
@@ -6,6 +6,8 @@ import Header from './components/Header'
 import Home from './components/Home'
 import Login from './components/login/Login'
 import {UserStorage} from './UserContext'
+import User from './components/user/User'
+import ProtectedRoute from './components/interface-elements/ProtectedRoute'
 
 const App = () => {
   return (
@@ -16,6 +18,9 @@ const App = () => {
           <Routes>
             <Route path='/' element={<Home/>}/>
             <Route path='/login/*' element={<Login/>}/>
+            <Route path='/conta/*' element={<ProtectedRoute>
+              <User/>
+            </ProtectedRoute>}/>
           </Routes>
         <Footer/>
       </UserStorage>
