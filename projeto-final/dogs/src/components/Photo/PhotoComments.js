51ff7a076770937a3e2c490f617ef8c812a86f87
ocesar9 <juliocesaoguimaraes@gmail.com>
2023-02-06 14:55:25 -0300

    Comentário Post

--- projeto-final/dogs/src/components/Photo/PhotoComments.js
+++ projeto-final/dogs/src/components/Photo/PhotoComments.js
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from 'react';
+import PhotoCommentsForm from './PhotoCommentsForm';
+import { UserContext } from '../../UserContext';
+import styles from './PhotoComments.module.css'
 
-const PhotoComments = () => {
+const PhotoComments = (props) => {
+  const [comments, setComments] = React.useState(() => props.comments);
+  const { login } = React.useContext(UserContext);
+
   return (
-    <div>PhotoComments</div>
-  )
-}
+    <>
+      <ul className={styles.comments}>
+        {comments.map((comment) => (
+          <li key={comment.comment_ID}>
+            <b>{comment.comment_author} :</b>
+            <span>{comment.comment_content}</span>
+          </li>
+        ))}
+      </ul>
+      {login && <PhotoCommentsForm id={props.id} setComments={setComments} />}
+    </>
+  );
+};
 
-export default PhotoComments
+export default PhotoComments;
---
+++ projeto-final/dogs/src/components/Photo/PhotoCommentsForm.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import { COMMENT_POST } from '../../api';
+import {ReactComponent as Enviar} from '../../Assets/enviar.svg';
+import useFetch from '../../hooks/useFetch';
+import Error from '../interface-elements/Error'
+
+const PhotoCommentsForm = ({id, setComments}) => {
+  
+  const [comment, setComment] = React.useState("");
+  const {request, error} = useFetch();
+
+  async function handleSubmit(event){
+    event.preventDefault();
+    const {url,options} = COMMENT_POST(id, {comment});
+    const {response,json} = await request(url, options);
+    console.log(json);
+    if(response.ok){
+      setComment('')
+      setComments((comments) => [...comments, json])
+    }
+
+  }
+
+  return(
+    <form onSubmit={handleSubmit}>
+      <textarea 
+      id='comment'
+      name='comment'
+      placeholder='Comente...'
+      value={comment} onChange={({target}) => setComment(target.value)}></textarea>
+      <button>
+        <Enviar/>
+      </button>
+      <Error error={error} />
+    </form>
+  )
+}
+
+export default PhotoCommentsForm
