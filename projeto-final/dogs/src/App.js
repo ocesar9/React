86d4f02ec9215a2745f43467d18313783bfff44f
ocesar9 <juliocesaoguimaraes@gmail.com>
2023-02-08 09:02:22 -0300

    Footer

--- projeto-final/dogs/src/App.js
+++ projeto-final/dogs/src/App.js
@@ -18,6 +18,7 @@ const App = () => {
       <BrowserRouter>
       <UserStorage>
         <Header/>
+        <main className='AppBody'>
           <Routes>
             <Route path='/' element={<Home/>}/>
             <Route path='login/*' element={<Login/>}/>
@@ -27,8 +28,8 @@ const App = () => {
             <Route path='foto/:id' element={<Photo />}/>
             <Route path='perfil/:user' element={<UserProfile />}/>
             <Route path='*' element={<NotFound />}/>
-
           </Routes>
+        </main>
         <Footer/>
       </UserStorage>
       </BrowserRouter>
